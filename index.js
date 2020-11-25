553c34c53e291bace7e1bdebbef46393dd908a4a
devamgupta92 <73942722+devamgupta92@users.noreply.github.com>
2020-11-25 17:29:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,20 +11,36 @@ const conn = new jsforce.Connection({
 const { SF_USERNAME, SF_PWD_TOKEN } = process.env;
 conn.login(SF_USERNAME,SF_PWD_TOKEN, function(err) {
     if (err) { return console.error(err); }
-    conn.query("SELECT Id,casenumber FROM Case", function(err, result) {
-        if (err) { 
-            return console.error(err); 
-        }
-        records = result.records;
+    conn.query("select id,name,payout_percent__c,sip_measure__c from sip_payout__c", function(err, result) {
+        if (err) { return console.error(err); }
         console.log("total : " + result.totalSize);
         console.log("fetched : " + result.records.length);
-    });
+        console.log("done ? : " + result.done);
+        records = result.records;
+        if(!result.done) {
+            // you can use the locator to fetch next records set.
+            // Connection#queryMore()
+            var temp = [];
+            conn.queryMore(result.nextRecordsUrl,function(err,res){
+                if (err) { return console.error(err); }
+                console.log("total : " + res.totalSize);
+                console.log("fetched : " + res.records.length);
+                console.log("done ? : " + res.done);
+                temp = res.records;
+                records = records.concat(temp);
+                console.log('finalsize++'+records.length);
+                //res.send(JSON.stringify(records));
+            })
+            console.log("next records URL : " + result.nextRecordsUrl);
+        }
+      });
+      
 });
 
 /*JS Force ends*/
 
 app.get("/",(req,res)=>{
-
+    
     res.send(JSON.stringify(records));
 });
 
